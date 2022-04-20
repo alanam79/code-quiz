de1f91d001319d620d555cfaec052c5af9fab45a
Jim Gryck <halo2382001@yahoo.com>
2022-04-20 12:04:34 -0400

    created HTML, CSS, and JS pages, created opening page and buttons that will lead to high score and the quiz

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+var highScoreEl = document.querySelector("#high-score-button");
+var startQuizEl = document.querySelector("#start-button");
+
+var quiz = function() {
+    // event.preventDefault();
+    // resetDisplay();    
+    alert("button clicked");
+};
+
+
+    // click for high score
+// highScoreEl.addEventListener("submit", function();
+
+    // cick to start quiz
+startQuizEl.addEventListener("click", quiz);
